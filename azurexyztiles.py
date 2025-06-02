60aa67fa5a3ae6a1d059877feca1a194be93b696
Andrew <54513661+44digits@users.noreply.github.com>
2025-06-02 12:04:58 -0700

    subclass of rasterioxyz for Azure reading/writing

---
+++ azurexyztiles.py
@@ -0,0 +1,50 @@
+"""
+AzureXYZtiles class based on rasterioxyz tiles
+    https://github.com/duncanmartyn/rasterioxyz
+
+Overloads write method to upload to Azure storage
+
+Date: 2025 Jun 01
+Author: Andrew
+"""
+
+import pathlib
+import rasterio.io
+import numpy
+import rasterioxyz
+import azure.storage.blob
+
+
+class AzureXYZtiles(rasterioxyz.Tiles):
+    '''base class on existing rasterioxyz library
+    '''
+    def write(self,
+            image_blobservice: azure.storage.blob.BlobServiceClient,
+            container: str,
+            directoryname: str):
+        '''override write method to write to azure
+        '''
+
+        driver = "PNG"
+        for tile in self.tiles:
+            out_dir = pathlib.Path(directoryname)
+            img_dir = out_dir.joinpath(str(tile.zoom), str(tile.column))
+            img_path = img_dir.joinpath(f"{tile.row}.{driver}")
+
+            if tile.data[-1].mean() == 0:
+                continue
+
+            with rasterio.io.MemoryFile() as memfile:
+                with memfile.open(
+                        driver=driver,
+                        width=self.pixels,
+                        height=self.pixels,
+                        count=tile.data.shape[0],
+                        dtype=numpy.uint8,
+                ) as dst:
+                    dst.write(tile.data)
+
+                blob_client = image_blobservice.get_blob_client(
+                        container=container,
+                        blob= str(img_path))
+                blob_client.upload_blob(memfile)
